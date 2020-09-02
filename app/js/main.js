2c8357cbc3926cc0f1ccb6a35523cfe45e05cf01
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-09-03 00:58:22 +0300

    added welcome and feature +adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,43 @@
 $(function () {
- 
+  $(".header__slider-items").slick({
+    arrows: false,
+    autoplay: true,
+  });
+  $(".provide__slider__items").slick({
+    arrows: true,
+    autoplay: true,
+    slidesToShow: 5,
+    /* Left btn */
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev icon-arrow-left">',
+    // /* Right btn */
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next icon-arrow-right">',
+    responsive: [
+      {
+        breakpoint: 1450,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1180,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
